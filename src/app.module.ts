285bd4a740a7d86a89916b71d94d97f955b2c81f
João Vitor Fernandes Canuto <joao678@gmail.com>
2022-08-21 20:13:55 -0300

    modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PersonController } from './Controllers/Person/Person.controller';
 import { PublicationController } from './Controllers/Publication/Publication.controller';
+import { RentalController } from './Controllers/Rental/Rental.controller';
 import { PersonService } from './Services/Person/Person.service';
 import { PublicationService } from './Services/Publication/Publication.service';
+import { RentalService } from './Services/Rental/Rental.service';
 
 @Module({
     imports: [],
-    controllers: [PersonController, PublicationController],
-    providers: [PersonService, PublicationService],
+    controllers: [PersonController, PublicationController, RentalController],
+    providers: [PersonService, PublicationService, RentalService],
 })
 export class AppModule { }
